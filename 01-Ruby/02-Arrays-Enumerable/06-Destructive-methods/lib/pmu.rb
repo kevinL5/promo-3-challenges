6e33adf39256fef3607345625b488c796d4a0df8
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-09 22:24:13 +0200

    Solved Exercice 1.2.6

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,15 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+
+  rank = race_array.size
+
+  for horse in 0...rank
+    race_array[horse] = "#{horse + 1}-#{race_array[horse]}!"
+  end
+
+  race_array.reverse!
+
 end
 
 
-
-
+puts pmu_format!(["Abricot du Laudot", "Black Caviar", "Brigadier Gerard"])
