7de76a45d93bbf980534da68f065d384ecbdf766
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-09 14:52:14 +0200

    Solved Exercice 1.2.2

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -9,6 +9,17 @@ begin
 
   # TODO: Add the name we just gotto the students array
 
+  students << name.to_s if name.to_s != ""
+
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+
+wagon_alphabetic = wagon_sort(students)
+
+puts "Congratulations! Your Wagon has #{students.size} students :"
+
+first_students = wagon_alphabetic[0..students.size-2].join(', ')
+final_greeting = "#{first_students} and #{wagon_alphabetic.last}."
+
+puts final_greeting
