6f48e40d42788ece71c6ed16e0f57faeccf8a45d
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-10 17:53:20 +0200

    Exercice 1.3.5

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,34 @@
 # Encoding: utf-8
 
+
+
+
 def louchebemize(sentence)
-  #TODO: implement your louchebem translator
+
+  words = sentence.split(' ')
+  translated_word = words.map {|word| louchebemize_word(word)}
+  translated_word.join(' ')
+
 end
+
+def louchebemize_word(word)
+  #TODO: implement your louchebem translator
+
+  suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+  vowels = %w(a e i o u y)
+
+
+  if word.length == 1
+    translated_word = word
+  elsif vowels.include? word[0]
+    translated_word = "l#{word}#{suffix.sample}"
+  elsif vowels.include? word[1]
+    translated_word = "l#{word[1..-1]}#{word[0]}#{suffix.sample}"
+  else
+    translated_word = "#{word[2..-2]}#{word[0..1]}#{suffix.sample}"
+  end
+  translated_word
+end
+
+
+louchebemize("bonjour salut holla")
