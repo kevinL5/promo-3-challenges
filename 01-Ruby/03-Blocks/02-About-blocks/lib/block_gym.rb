ebfef0e8d58645ffe9fb7144b86f454c477321db
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-10 13:25:20 +0200

    Solved Exercice 1.3.2

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,12 +1,23 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  if attr == nil
+    "<#{tag_name}>#{yield}</#{tag_name}>"
+  else
+    "<#{tag_name} #{attr[0]}='#{attr[1]}'>#{yield}</#{tag_name}>"
+  end
 end
+
+tag("h1", attr = ["id", "title"]) { "Some code" }
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  start = Time.now
+  yield
+  Time.now - start
 end
 
 def transform(element)
   #TODO:  Simply execute the given block on element
+  yield(element)
 end
