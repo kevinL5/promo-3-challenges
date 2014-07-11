05a258e9e79d5fdfaf805c63cb34a90e9778a141
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-11 11:30:53 +0200

    Exercice rake

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,52 @@
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+  domc = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170
+}
+
+  calories = domc[burger] + domc[side] + domc[beverage]
 end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+  domc = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170,
+    #"Happy Meal" => domc["Cheese Burger" ] + domc["French fries"] + domc["Coca"]
+    "Happy Meal" => 580
+    }
+
+
+
+  product_calories = 0
+
+  orders.each do |product|
+    product_calories += domc[product]
+  end
+  product_calories
 end
+
+puts poor_calories_counter("Big Mac", "French fries", "Coca")
+puts calories_counter("Coca", "Happy Meal")
+
+
+
+
+
+
+
+
+
