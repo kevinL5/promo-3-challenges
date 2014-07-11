1b36dd65e6f1f59f6963c0cee7c0ef013954a709
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-11 09:17:54 +0200

    Solved Exercice 1.3.5 HEY

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -14,18 +14,42 @@ def louchebemize_word(word)
 
   suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
   vowels = %w(a e i o u y A E I O U Y)
+  sym = [",", "!", "?", ":", ";"]
 
 
-  if word.length == 1
-    translated_word = word
-  elsif vowels.include? word[0]
-    translated_word = "l#{word}#{suffix.sample}"
-  elsif vowels.include? word[1]
-    translated_word = "l#{word[1..-1]}#{word[0]}#{suffix.sample}"
+
+
+  if sym.include? word[-2]
+    symb_2 = word[-2..-1]
+    if vowels.include? word[0]
+      translated_word = "l#{word[0..-3]}#{suffix.sample}#{symb_2}"
+    elsif vowels.include? word[1]
+      translated_word = "l#{word[1..-3]}#{word[0]}#{suffix.sample}#{symb_2}"
+    else
+      translated_word = "l#{word[2..-3]}#{word[0..1]}#{suffix.sample}#{symb_2}"
+    end
+
+  elsif sym.include? word[-1]
+    symb_1 = word[-1]
+    if vowels.include? word[0]
+      translated_word = "l#{word[0..-2]}#{suffix.sample}#{symb_1}"
+    elsif vowels.include? word[1]
+      translated_word = "l#{word[1..-2]}#{word[0]}#{suffix.sample}#{symb_1}"
+    else
+      translated_word = "l#{word[2..-2]}#{word[0..1]}#{suffix.sample}#{symb_1}"
+    end
+
   else
-    translated_word = "l#{word[2..-1]}#{word[0..1]}#{suffix.sample}"
+    if word.length == 1
+    translated_word = word
+    elsif vowels.include? word[0]
+      translated_word = "l#{word}#{suffix.sample}"
+    elsif vowels.include? word[1]
+      translated_word = "l#{word[1..-1]}#{word[0]}#{suffix.sample}"
+    else
+      translated_word = "l#{word[2..-1]}#{word[0..1]}#{suffix.sample}"
+    end
   end
-  translated_word
 end
 
 
