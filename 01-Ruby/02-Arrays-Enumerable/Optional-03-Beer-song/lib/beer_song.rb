d39c240938809cee05668485e856eb90f688399a
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-14 17:58:18 +0200

    Exercice Solved 1.2.optional3

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,14 @@
-def beersong(nb_at_start)
-  #TODO: sing the song
+nb_at_start = ARGV[0].to_i
+
+if nb_at_start == ''
+  puts "Puts a argument"
+else
+    puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+    until nb_at_start == 1
+      nb_at_start -= 1
+      puts "Take one down, pass it around, #{nb_at_start} bottles of beer on the wall!
+  end"
+      puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+    end
+    puts "Take one down, pass it around, no more bottles of beer"
 end
