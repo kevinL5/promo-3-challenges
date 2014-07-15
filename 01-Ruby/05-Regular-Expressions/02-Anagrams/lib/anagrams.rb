6af56842b1af3695d7428944e475fc75412eb3ea
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-15 15:25:07 +0200

    Solved Exercice 1.5.2 with hash

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -10,31 +10,26 @@ end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
-  hash = {}
+  hash_1 = {}
+  hash_2 = {}
 
-  answer = false
+  a_string_new = a_string.downcase.scan(/\w/)
+  another_string_new = another_string.downcase.scan(/\w/)
 
-  a_string_new = a_string.downcase.gsub(/\W/, '')
-  another_string_new = another_string.downcase.gsub(/\W/, '')
-
-  another_string_new.split('').each do |letter|
-    hash[letter] = letter
+  a_string_new.each do |letter|
+  hash_1[letter.to_sym] = a_string_new.count(letter)
   end
 
-  if a_string_new.size == another_string_new.size
-    another_string_new.split('').each do |letter_ste|
-      answer = true if hash[letter_ste] == letter_ste
-    end
-    answer
+  another_string_new.each do |letter|
+  hash_2[letter.to_sym] = a_string_new.count(letter)
   end
-  answer
+
+  hash_1 == hash_2
 end
 
+n = 500000
 
-n = 50000
-text = File.read("spec/anagrams_spec.rb")
-other_text = text.reverse
 Benchmark.bm do |x|
-  x.report { n.times { anagrams?(text, other_text) } }
-  x.report { n.times { anagrams_on_steroids?(text, other_text) } }
+  x.report { n.times { anagrams?('meuf', 'fume') } }
+  x.report { n.times { anagrams_on_steroids?('meuf', 'fume') } }
 end
