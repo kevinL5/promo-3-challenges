63bbff15d36cd1431d5a4f84a043aa38d4b7c49d
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-17 11:22:09 +0200

    Solved Exercice 2.2.1

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,34 @@
 class Dessert
+  attr_reader :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
+    @delicious = true
   end
-  
+
+  def healthy?
+    if @calories < 200
+      true
+    else
+      false
+    end
+  end
+
   def delicious?
+    @delicious
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+  attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    @flavor == "black licorice" ? false : true
   end
 end
