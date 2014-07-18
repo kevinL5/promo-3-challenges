af4b63ca1b3705ec62a4862171719de6438d7c52
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-18 15:53:16 +0200

    In progress

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
@@ -1,6 +1,30 @@
 require_relative 'cookbook'
 
 class Controller
+  def initialize
+    @ui = Ui.new
+    @cookbook = Cookbook.new
+  end
+
+  def display_recipes
+    recipes = @ui.tasks
+    @interface.display_tasks(tasks)
+  end
+
+  def add_recipe
+    description = @interface.ask_user_for_task_description
+    task = Task.new(description)
+    @repository.add_task(task)
+  end
+
+  def delete_recipe
+    index = @interface.ask_user_for_task_id_to_mask_as_done
+    task = @repository.find_task(index)
+    task.mark_as_done
+  end
+end
+
+class Controller
   def initialize(file)
     # Here you should instantiate the Cookbook model with the file
   end
--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
@@ -1,9 +1,52 @@
+require 'csv'
+require 'awesome_print'
+
 class Cookbook
+  attr_reader :list_recipes
+
   def initialize(file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    @file = file
+    @list_recipes = []
+    retrieve
   end
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
   # TODO: Implement a save method that will write the data into the CSV
   # And don't forget to use this save method when you have to modify something in your recipes array.
+
+  def retrieve
+    CSV.foreach(@file) do |row|
+      @list_recipes << row[0]
+    end
+  end
+
+
+  def create(recipe)
+    @list_recipes << recipe
+
+    save
+  end
+
+  def destroy(recipe)
+    @list_recipes.delete(recipe)
+
+    save
+  end
+
+  def save
+    CSV.open(@file, 'w') do |csv|
+      @list_recipes.each do |recipe|
+        csv << [recipe]
+      end
+    end
+  end
+
 end
+
+cookbook = Cookbook.new('recipes.csv')
+cookbook.create('Fish')
+cookbook.create('Chips')
+cookbook.destroy('Fish')
+
+#ap cookbook.list_recipes
