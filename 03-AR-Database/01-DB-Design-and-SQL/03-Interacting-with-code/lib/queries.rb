0e44269376a38e4475c7dace260a7046fa4b90c8
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-24 15:32:42 +0200

    Solved DB

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -6,16 +6,27 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
+  array_count = db.execute "SELECT COUNT(*) FROM #{table_name}"
+  array_count [0] [0]
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+  array_artists = db.execute "SELECT Name FROM Artist ORDER BY Name ASC"
+  array_artists.flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+  array_love_song = db.execute "SELECT Name FROM Track WHERE Name LIKE '%love%'"
+  array_love_song.flatten
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
+  time = min_length * 60000
+  array_long_tracks = db.execute "SELECT Name FROM Track WHERE Milliseconds > #{time} ORDER BY Milliseconds DESC"
+  array_long_tracks.flatten
 end
+
+p sorted_artists(db)
