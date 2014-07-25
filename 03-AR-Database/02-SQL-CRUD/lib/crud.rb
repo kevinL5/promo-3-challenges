5b2ccca52d0c828c11b62001da88ba6004b3db13
Kevin Lemaire <kevin.lemairecolombel@gmail.com>
2014-07-25 12:28:15 +0200

    Completely solved exercice

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -2,31 +2,56 @@ require 'sqlite3'
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{ CREATE TABLE posts
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(255),
+      source_url VARCHAR(255),
+      date DATE,
+      rating INT(1)
+      )
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
+end
+
+def display(row)
+  puts "ID : #{row[0]}, Name : #{row[1]}, Url : #{row[2]}, Date : #{row[3]}, Rate : #{row[4]} / 10"
 end
 
 def create_post(db, post)
   #TODO: add a new post to your db
+  db.execute "INSERT INTO posts (name, source_url, date, rating)
+            VALUES ('#{post[:name]}', '#{post[:source_url]}', '#{post[:date]}', #{post[:rating]})"
 end
 
 def get_posts(db)
    #TODO: list all posts
+  stm = db.execute "SELECT * FROM posts"
+  stm.each { |row| display(row) }
 end
 
 def get_post(db, id)
   #TODO: get a specific post
+  row = db.execute "SELECT * FROM posts WHERE id = #{id}"
+  row = row.flatten
+  display(row)
 end
 
 def update_post(db, id, name)
 	#TODO: update a post's name in your db
+  db.execute "UPDATE posts SET name = '#{name}' WHERE id = #{id}"
+  row = db.execute "SELECT * FROM posts WHERE id = #{id}"
+  row = row.flatten
+  display(row)
 end
 
 def delete_posts(db)
   #TODO: delete a post in your db
+  db.execute "DELETE FROM posts"
+  db.execute "DELETE FROM sqlite_sequence WHERE name = 'posts'"
 end
 
 def delete_post(db, id)
   #TODO: delete a specific post in your db
+  db.execute "DELETE FROM posts WHERE id = #{id}"
 end
--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -5,22 +5,31 @@ def ask_and_get(param)
   gets.chomp
 end
 
+def ask_and_get_i(param)
+  puts "What is the #{param} of the post?"
+  gets.chomp.to_i
+end
+
 db_path = "db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+#create_scheme(db)
 
 while true
 
-  puts "Hey you, what do you want to do today? Enter <task_id>"
+  puts "\nHey you, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
-  puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "3. Read a specific post"
+  puts "4. Update a post name"
+  puts "5. Delete all posts"
+  puts "6. Delete a specific post"
+  puts "7. Exit"
+
   # TODO: add other CRUD tasks if you wish!
-	choice =  gets.chomp.to_i
-	
+  choice =  gets.chomp.to_i
+  puts "----------------------"
+
 	case choice
   when 1
     name = ask_and_get("name")
@@ -31,9 +40,20 @@ while true
   when 2
     get_posts(db)
   when 3
+    id = ask_and_get_i("id")
+    get_post(db, id)
+  when 4
+    name = ask_and_get("name")
+    id = ask_and_get_i("id")
+    update_post(db, id, name)
+  when 5
     delete_posts(db)
-  when 4 
+  when 6
+    id = ask_and_get_i("id")
+    delete_post(db, id)
+  when 7
     break
-	end 
-	
+	end
+  puts "----------------------"
+
 end
